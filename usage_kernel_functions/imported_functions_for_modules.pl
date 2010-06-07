c066de149be92be7b8f24ec22d9df65c45ef671a
Andrey Tsyvarev <tsyvarev@ispras.ru>
2010-06-07 18:57:58 +0400

    Вывод без ":". Задокументирован случай, когда 'modprobe -l' возвращает относительный путь модуля.

--- usage_kernel_functions/imported_functions_for_modules.pl
+++ usage_kernel_functions/imported_functions_for_modules.pl
@@ -8,6 +8,9 @@ for my $module (@ARGV)
 		print("Cannot find module $module.\n");
 		next;
 	}
+    #On some systems `modprobe -l` return relative path.
+    #If it is your case, uncomment next line.
+    #$module_path = "/lib/modules/`uname -r`/$module_path";
 	my @functions_local = `perl imported_functions.pl $module_path`;
 	for my $function (@functions_local)
 	{
@@ -25,6 +28,5 @@ for my $module (@ARGV)
 my @functions_sorted = sort {$functions{$b} <=> $functions{$a}} keys %functions;
 for my $function (@functions_sorted)
 {
-	printf("%-25s: %d\n", $function, $functions{$function});
-	#print("$function:\t\t\t$functions{$function}\n");
+	printf("%-30s %d\n", $function, $functions{$function});
 }
