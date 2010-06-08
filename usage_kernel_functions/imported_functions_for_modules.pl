3f4ee50a8a6bcf079aca0b97dc83a666d6fad81a
Andrey Tsyvarev <tsyvarev@ispras.ru>
2010-06-08 19:17:00 +0400

    Несколько улучшений скрипта для получения списка используемых функций ядра в плане кроссплатформенности.

--- usage_kernel_functions/imported_functions_for_modules.pl
+++ usage_kernel_functions/imported_functions_for_modules.pl
@@ -1,32 +1,36 @@
 my %functions = ();
 for my $module (@ARGV)
 {
-	my $module_path = `modprobe -l $module`;
-	chomp $module_path;
-	if($module_path eq "")
-	{
-		print("Cannot find module $module.\n");
-		next;
-	}
+    #Regular path to modprobe is /sbin/modprobe, but on some systems /sbin is not in $PATH
+    my $module_path = `PATH=\$PATH:/sbin modprobe -l $module`;
+    chomp $module_path;
+    if($module_path eq "")
+    {
+        print(STDERR "Cannot find module $module.\n");
+        next;
+    }
     #On some systems `modprobe -l` return relative path.
-    #If it is your case, uncomment next line.
-    #$module_path = "/lib/modules/`uname -r`/$module_path";
-	my @functions_local = `perl imported_functions.pl $module_path`;
-	for my $function (@functions_local)
-	{
-		chomp $function;
-		if($functions{$function})
-		{
-			$functions{$function} = $functions{$function} + 1;
-		}
-		else
-		{
-			$functions{$function} = 1;
-		}
-	}
+    if($module_path =~ /^[^\/]/)
+    {
+        $module_path = "/lib/modules/`uname -r`/$module_path";
+    }
+    
+    my @functions_local = `perl imported_functions.pl $module_path`;
+    for my $function (@functions_local)
+    {
+        chomp $function;
+        if($functions{$function})
+        {
+            $functions{$function} = $functions{$function} + 1;
+        }
+        else
+        {
+            $functions{$function} = 1;
+        }
+    }
 }
 my @functions_sorted = sort {$functions{$b} <=> $functions{$a}} keys %functions;
 for my $function (@functions_sorted)
 {
-	printf("%-30s %d\n", $function, $functions{$function});
+    printf("%-30s %d\n", $function, $functions{$function});
 }
