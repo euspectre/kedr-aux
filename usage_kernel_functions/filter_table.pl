db57d22bb37023a6d48c1ba26ccf6818d2a8e1bc
Andrey Tsyvarev <tsyvarev@ispras.ru>
2010-06-16 15:33:54 +0400

    For filter_table.sh added support for multiple exclude files.
    Example, included to the README, shows how to additionally filter already covered functions.

--- usage_kernel_functions/filter_table.pl
+++ usage_kernel_functions/filter_table.pl
@@ -1,28 +1,31 @@
-# filter_table.pl file_with_excludes
+# filter_table.pl files_with_excludes ..
 #
 # Filter functions usage statictic.
 #
 # Accept table with statistic in INPUT stream, and out the same table,
-# but without functions, listed in 'file_with_excludes'.
+# but without functions, listed in 'files_with_excludes' files.
 #
 # Excluded functions should be listed in the file each at the new line.
 # Surrounding space are stripped.
 # Empty lines and lines started with '#' are ignored.
 
-$usage_string = "Usage:\n\n\tfilter_table.pl file_with_excludes\n\n";
-
-$#ARGV == 0 or die($usage_string);
-my $exclude_filename = shift @ARGV;
-open(my $exclude_file, $exclude_filename) 
-	or die("Cannot open file '$exclude_filename' for read.");
+$usage_string = "Usage:\n\n\tfilter_table.pl files_with_excludes [..]\n\n";
 
 my %functions_exclude = ();
-for my $line (<$exclude_file>)
+
+$#ARGV >= 0 or die($usage_string);
+for my $exclude_filename (@ARGV)
 {
-	$line =~ s/^\s*(\S*)\s*$/\1/;
-	$line ne "" or next;
-	$line !~ /^#/ or next;
-	$functions_exclude{$line} = "exclude";
+    open(my $exclude_file, $exclude_filename) 
+    	or die("Cannot open file '$exclude_filename' for read.");
+
+    for my $line (<$exclude_file>)
+    {
+    	$line =~ s/^\s*(\S*)\s*$/\1/;
+    	$line ne "" or next;
+    	$line !~ /^#/ or next;
+    	$functions_exclude{$line} = "exclude";
+    }
 }
 #shift header line(how??)
 #print(readline);
