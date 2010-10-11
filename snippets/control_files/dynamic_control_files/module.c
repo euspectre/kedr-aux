ddc5532874acc636ba4189c5e3fe7dd6aaa14bec
Andrey Tsyvarev <tsyvarev@ispras.ru>
2010-10-11 21:54:28 +0400

    Simplify creation of the control file via macro.

--- snippets/control_files/dynamic_control_files/module.c
+++ snippets/control_files/dynamic_control_files/module.c
@@ -3,13 +3,13 @@
 
 #include <linux/module.h>
     
-#include <linux/uaccess.h> /* copy_*_user functions */
-
 #include <linux/mutex.h>
 
 #include <linux/debugfs.h> /* control file will be create on debugfs*/
 
 #include <linux/string.h> /* strlen, strcpy */
+
+#include "control_file.h"
 
 MODULE_AUTHOR("Tsyvarev");
 MODULE_LICENSE("GPL");
@@ -39,8 +39,6 @@ static const char* object_name_default = "noname";
 //Protect generation and its object from concurrent read and write.
 DEFINE_MUTEX(generation_mutex);
 
-
-
 /*
  * Directory which will contatins our files(in our case - 1 file).
  */
@@ -60,231 +58,17 @@ static const char* get_object_name_internal(struct generation* g);
 static int set_object_name_internal(struct generation* g, const char* new_name);
 
 ///////////////////////////File operations///////////////////////
+//Static control file
+char* generation_file_get_str(struct inode* inode);
+int generation_file_set_str(const char* str, struct inode* inode);
 
-// Helper functions, which used in implementation of file operations
+CONTROL_FILE_OPS(generation_file_operations, generation_file_get_str, generation_file_set_str);
 
-//Helper for read operation of file. 'As if' it reads file, contatining string.
-static ssize_t string_operation_read(const char* str, char __user *buf, size_t count, 
-	loff_t *f_pos);
+//Dynamic control files
+char* object_name_file_get_str(struct inode* inode);
+int object_name_file_set_str(const char* str, struct inode* inode);
 
-//Helper for llseek operation of file. Help to user space utilities to find out size of file.
-static loff_t string_operation_llseek (const char* str, loff_t *f_pos, loff_t offset, int whence);
-
-// Helper function for write operation of file. Allocate buffer, which contain writting string.
-// On success(non-negative value is returned), out_str should be freed when no longer needed.
-static ssize_t
-string_operation_write(char** out_str, const char __user *buf,
-    size_t count, loff_t *f_pos);
-
-
-
-/////////////Implementation of file operations/////////////////////////
-/*
- * For prevent unspecified behaviour of default implementation of llseek,
- * define our one.
- *
- * It may be used by user-space utils for determine size of file.
- */
-static loff_t
-generation_file_llseek(struct file *filp, loff_t off, int whence)
-{
-    loff_t result;
-    struct generation* g;
-
-    if(mutex_lock_killable(&generation_mutex))
-    {
-        pr_debug("llseek() operation was killed.");
-        return -EAGAIN;
-    }
-    g = generation_get_internal();
-    result = string_operation_llseek(g ? g->generation_name : generation_name_absent,
-        &filp->f_pos, off, whence);
-        
-    mutex_unlock(&generation_mutex);
-    
-    return result;
-}
-
-/*
- * Get current characteristic of the module.
- *
- * In our case - return name of object.
- */
-static ssize_t 
-generation_file_read(struct file *filp, char __user *buf, size_t count, 
-	loff_t *f_pos)
-{
-    ssize_t result;
-    struct generation* g;
-    
-    if(mutex_lock_killable(&generation_mutex))
-    {
-        pr_debug("read() operation was killed.");
-        return -EAGAIN;
-    }
-    
-    g = generation_get_internal();
-    result = string_operation_read(g ? g->generation_name : generation_name_absent,
-        buf, count, f_pos);
-        
-    mutex_unlock(&generation_mutex);
-    
-    return result;
-}
-
-/*
- *
- */
-static ssize_t 
-generation_file_write(struct file *filp, const char __user *buf,
-    size_t count, loff_t *f_pos)
-{
-    char* write_str;
-
-    ssize_t result = string_operation_write(&write_str, buf, count, f_pos);
-    
-    if(result < 0) return result;//error
-
-    if(mutex_lock_killable(&generation_mutex))
-    {
-        pr_debug("write() operation was killed.");
-        kfree(write_str);
-        return -EAGAIN;
-    }
-    if(strcmp(write_str, generation_name_absent) == 0)
-    {
-        generation_destroy_internal();
-        result = 0;
-    }
-    else
-    {
-        result = generation_create_internal(write_str);
-    }
-    
-    mutex_unlock(&generation_mutex);
-
-    kfree(write_str);
-    return result ? result : count;
-}
-
-static struct file_operations generation_file_operations =
-{
-    .owner = THIS_MODULE,//prevent module from unload while file is opened.
-    .llseek = generation_file_llseek, //size
-    .read = generation_file_read, //get name of current generation
-    .write = generation_file_write, //create new generation instead current
-};
-///////////////Implementation of dynamic control file operations
-
-/*
- * Each operation, which need generation object(this object is create this file),
- * should get this object from private data of inode.
- *
- * NULL pointer in this field means that generation object is already destroyed,
- * so file is meaningless.
- *
- * Note, that only direct reference to these private data should be used, without cache in 'filp'.
- */
-
-/*
- * For prevent unspecified behaviour of default implementation of llseek,
- * define our one.
- *
- * It may be used by user-space utils for determine size of file.
- */
-static loff_t
-object_name_file_llseek(struct file *filp, loff_t off, int whence)
-{
-    loff_t result;
-    struct generation* g;
-
-    if(mutex_lock_killable(&generation_mutex))
-    {
-        pr_debug("llseek() operation was killed.");
-        return -EAGAIN;
-    }
-
-    g = filp->f_dentry->d_inode->i_private;
-    if(g)
-        result = string_operation_llseek(get_object_name_internal(g), &filp->f_pos, off, whence);
-    else
-        result = -EINVAL;// really, file should be already destroyed
-        
-    mutex_unlock(&generation_mutex);
-    
-    return result;
-}
-
-/*
- * Get current characteristic of the module.
- *
- * In our case - return name of object.
- */
-static ssize_t 
-object_name_file_read(struct file *filp, char __user *buf, size_t count, 
-	loff_t *f_pos)
-{
-    ssize_t result;
-    struct generation* g;
-    
-    if(mutex_lock_killable(&generation_mutex))
-    {
-        pr_debug("read() operation was killed.");
-        return -EAGAIN;
-    }
-
-    g = filp->f_dentry->d_inode->i_private;
-    if(g)
-        result = string_operation_read(get_object_name_internal(g), buf, count, f_pos);
-    else
-        result = -EINVAL;// really, file should be already destroyed
-        
-    mutex_unlock(&generation_mutex);
-    
-    return result;
-}
-
-/*
- * Force module from user space to do something.
- *
- * In our case - set name of object to string, which is written.
- */
-static ssize_t 
-object_name_file_write(struct file *filp, const char __user *buf,
-    size_t count, loff_t *f_pos)
-{
-    char* write_str;
-    struct generation* g;
-
-    ssize_t result = string_operation_write(&write_str, buf, count, f_pos);
-    
-    if(result < 0) return result;//error
-
-    if(mutex_lock_killable(&generation_mutex))
-    {
-        pr_debug("write() operation was killed.");
-        kfree(write_str);
-        return -EAGAIN;
-    }
-    g = filp->f_dentry->d_inode->i_private;
-    if(g)
-        result = set_object_name_internal(g, write_str);
-    else
-        result = -EINVAL;// really, file should be already destroyed
-    
-    mutex_unlock(&generation_mutex);
-
-    kfree(write_str);
-    return result ? result : count;
-}
-
-static struct file_operations object_name_file_operations =
-{
-    .owner = THIS_MODULE,//prevent module from unload while file is opened.
-    .llseek = object_name_file_llseek, //size
-    .read = object_name_file_read, //get current name
-    .write = object_name_file_write, //set current name
-};
+CONTROL_FILE_OPS(object_name_file_operations, object_name_file_get_str, object_name_file_set_str);
 /////////////////////////////////////////////////////////////////////////////
 
 static int __init
@@ -326,6 +110,103 @@ module_init(this_module_init);
 module_exit(this_module_exit);
 
 ////////////////////Implementation of auxiliary functions////////////////////////////////
+
+//Static control file
+char* generation_file_get_str(struct inode* inode)
+{
+    char* str;
+    struct generation* g;
+    
+    if(mutex_lock_killable(&generation_mutex))
+    {
+        pr_debug("operation was killed.");
+        return NULL;
+    }
+    
+    g = generation_get_internal();
+    str = kstrdup(g ? g->generation_name : generation_name_absent, GFP_KERNEL);
+        
+    mutex_unlock(&generation_mutex);
+    
+    return str;
+}
+int generation_file_set_str(const char* str, struct inode* inode)
+{
+    int result;
+    if(mutex_lock_killable(&generation_mutex))
+    {
+        pr_debug("operation was killed.");
+        return -EAGAIN;
+    }
+    if(strcmp(str, generation_name_absent) == 0)
+    {
+        generation_destroy_internal();
+        result = 0;
+    }
+    else
+    {
+        result = generation_create_internal(str);
+    }
+    
+    mutex_unlock(&generation_mutex);
+
+    return result;
+}
+
+//Dynamic control files
+
+/*
+ * Each operation, which need generation object(this object is create this file),
+ * should get this object from private data of inode.
+ *
+ * NULL pointer in this field means that generation object is already destroyed,
+ * so file is meaningless.
+ *
+ * Note, that only direct reference to these private data should be used, without cache in 'filp'.
+ */
+
+char* object_name_file_get_str(struct inode* inode)
+{
+    char* str;
+    struct generation* g;
+
+    if(mutex_lock_killable(&generation_mutex))
+    {
+        pr_debug("operation was killed.");
+        return NULL;
+    }
+
+    g = inode->i_private;
+    if(g)
+        str = kstrdup(get_object_name_internal(g), GFP_KERNEL);
+    else
+        str = NULL;// really, file should be already destroyed
+        
+    mutex_unlock(&generation_mutex);
+    
+    return str;
+}
+int object_name_file_set_str(const char* str, struct inode* inode)
+{
+    struct generation* g;
+    int result;
+    
+    if(mutex_lock_killable(&generation_mutex))
+    {
+        pr_debug("write() operation was killed.");
+        return -EAGAIN;
+    }
+    g = inode->i_private;
+    if(g)
+        result = set_object_name_internal(g, str);
+    else
+        result = -EINVAL;// really, file should be already destroyed
+    
+    mutex_unlock(&generation_mutex);
+
+    return result;
+}
+/////////////////////////////////////////////////////////////
 
 struct generation* generation_get_internal(void)
 {
@@ -415,107 +296,4 @@ static int set_object_name_internal(struct generation* g, const char* new_name)
     g->object_name = name;
     
     return 0;
-}
-
-
-//Helper for read operation of file. 'As if' it reads file, contatining string.
-ssize_t string_operation_read(const char* str, char __user *buf, size_t count, 
-	loff_t *f_pos)
-{
-    //length of 'file'(include terminating '\0')
-    size_t size = strlen(str) + 1;
-    //whether position out of range
-    if((*f_pos < 0) || (*f_pos > size)) return -EINVAL;
-    if(*f_pos == size) return 0;// eof
-
-    if(count + *f_pos > size)
-        count = size - *f_pos;
-    if(copy_to_user(buf, str + *f_pos, count) != 0)
-        return -EFAULT;
-    
-    *f_pos += count;
-    return count;
-}
-
-//Helper for llseek operation of file. Help to user space utilities to find out size of file.
-loff_t string_operation_llseek (const char* str, loff_t *f_pos, loff_t offset, int whence)
-{
-    loff_t new_offset;
-    size_t size = strlen(str) + 1;
-    switch(whence)
-    {
-    case 0: /* SEEK_SET */
-        new_offset = offset;
-    break;
-    case 1: /* SEEK_CUR */
-        new_offset = *f_pos + offset;
-    break;
-    case 2: /* SEEK_END */
-        new_offset = size + offset;
-    break;
-    default: /* can't happen */
-        return -EINVAL;
-    };
-    if(new_offset < 0) return -EINVAL;
-    if(new_offset > size) new_offset = size;//eof
-    
-    *f_pos = new_offset;
-    //returning value is offset from the beginning, filp->f_pos, generally speaking, may be any.
-    return new_offset;
-}
-
-ssize_t
-string_operation_write(char** out_str, const char __user *buf,
-    size_t count, loff_t *f_pos)
-{
-    char* buffer;
-
-    if(count == 0)
-    {
-        pr_err("write: 'count' shouldn't be 0.");
-        return -EINVAL;
-    }
-
-    /*
-     * Feature of control files.
-     *
-     * Because writting to such files is really command to the module to do something,
-     * and successive reading from this file return total effect of this command.
-     * it is meaningless to process writting not from the start.
-     *
-     * In other words, writting always affect to the global content of the file.
-     */
-    if(*f_pos != 0)
-    {
-        pr_err("Partial rewritting is not allowed.");
-        return -EINVAL;
-    }
-    //Allocate buffer for writting value - for its preprocessing.
-    buffer = kmalloc(count + 1, GFP_KERNEL);
-    if(buffer == NULL)
-    {
-        pr_err("Cannot allocate buffer.");
-        return -ENOMEM;
-    }
-
-    if(copy_from_user(buffer, buf, count) != 0)
-    {
-        pr_err("copy_from_user return error.");
-        kfree(buffer);
-        return -EFAULT;
-    }
-    // For case, when one try to write not null-terminated sequence of bytes,
-    // or omit terminated null-character.
-    buffer[count] = '\0';
-
-    /*
-     * Usually, writting to the control file is performed via 'echo' command,
-     * which append new-line symbol to the writting string.
-     *
-     * Because, this symbol is usually not needed, we trim it.
-     */
-    if(buffer[count - 1] == '\n') buffer[count - 1] = '\0';
-
-    *out_str = buffer;
-    return count;
 }
--- snippets/control_files/static_control_files/module.c
+++ snippets/control_files/static_control_files/module.c
@@ -3,13 +3,13 @@
 
 #include <linux/module.h>
     
-#include <linux/uaccess.h> /* copy_*_user functions */
-
 #include <linux/mutex.h>
 
 #include <linux/debugfs.h> /* control file will be create on debugfs*/
 
 #include <linux/string.h> /* strlen, strcpy */
+
+#include "control_file.h"
 
 MODULE_AUTHOR("Tsyvarev");
 MODULE_LICENSE("GPL");
@@ -73,108 +73,10 @@ static int set_object_name_internal(const char* new_name);
 
 
 ///////////////////////////File operations///////////////////////
+static char* object_name_get_str(struct inode* inode);
+static int object_name_set_str(const char* str, struct inode* inode);
 
-// Helper functions, which used in implementation of file operations
-
-//Helper for read operation of file. 'As if' it reads file, contatining string.
-static ssize_t string_operation_read(const char* str, char __user *buf, size_t count, 
-	loff_t *f_pos);
-
-//Helper for llseek operation of file. Help to user space utilities to find out size of file.
-static loff_t string_operation_llseek (const char* str, loff_t *f_pos, loff_t offset, int whence);
-
-// Helper function for write operation of file. Allocate buffer, which contain writting string.
-// On success(non-negative value is returned), out_str should be freed when no longer needed.
-static ssize_t
-string_operation_write(char** out_str, const char __user *buf,
-    size_t count, loff_t *f_pos);
-
-
-
-/////////////Implementation of file operations/////////////////////////
-/*
- * For prevent unspecified behaviour of default implementation of llseek,
- * define our one.
- *
- * It may be used by user-space utils for determine size of file.
- */
-static loff_t
-object_name_file_llseek(struct file *filp, loff_t off, int whence)
-{
-    loff_t result;
-
-    if(mutex_lock_killable(&object_name_mutex))
-    {
-        pr_debug("llseek() operation was killed.");
-        return -EAGAIN;
-    }
-    result = string_operation_llseek(get_object_name_internal(), &filp->f_pos, off, whence);
-        
-    mutex_unlock(&object_name_mutex);
-    
-    return result;
-}
-
-/*
- * Get current characteristic of the module.
- *
- * In our case - return name of object.
- */
-static ssize_t 
-object_name_file_read(struct file *filp, char __user *buf, size_t count, 
-	loff_t *f_pos)
-{
-    ssize_t result;
-    
-    if(mutex_lock_killable(&object_name_mutex))
-    {
-        pr_debug("read() operation was killed.");
-        return -EAGAIN;
-    }
-    result = string_operation_read(get_object_name_internal(), buf, count, f_pos);
-        
-    mutex_unlock(&object_name_mutex);
-    
-    return result;
-}
-
-/*
- * Force module from user space to do something.
- *
- * In our case - set name of object to string, which is written.
- */
-static ssize_t 
-object_name_file_write(struct file *filp, const char __user *buf,
-    size_t count, loff_t *f_pos)
-{
-    char* write_str;
-
-    ssize_t result = string_operation_write(&write_str, buf, count, f_pos);
-    
-    if(result < 0) return result;//error
-
-    if(mutex_lock_killable(&object_name_mutex))
-    {
-        pr_debug("write() operation was killed.");
-        kfree(write_str);
-        return -EAGAIN;
-    }
-    result = set_object_name_internal(write_str);
-    mutex_unlock(&object_name_mutex);
-
-    kfree(write_str);
-    return result ? result : count;
-}
-
-static struct file_operations object_name_file_operations =
-{
-    .owner = THIS_MODULE,//prevent module from unload while file is opened.
-    .llseek = object_name_file_llseek, //size
-    .read = object_name_file_read, //get current name
-    .write = object_name_file_write, //set current name
-};
-
-
+CONTROL_FILE_OPS(object_name_file_operations, object_name_get_str, object_name_set_str);
 /////////////////////////////////////////////////////////////////////////////
 
 static int __init
@@ -224,10 +126,36 @@ module_exit(this_module_exit);
 
 ////////////////////Implementation of auxiliary functions////////////////////////////////
 
+char* object_name_get_str(struct inode* inode)
+{
+    char* result;
+    if(mutex_lock_killable(&object_name_mutex))
+    {
+        pr_debug("was killed.");
+        return NULL;
+    }
+    result = kstrdup(get_object_name_internal(), GFP_KERNEL);
+    mutex_unlock(&object_name_mutex);
+    return result;
+}
+int object_name_set_str(const char* str, struct inode* inode)
+{
+    int result;
+    if(mutex_lock_killable(&object_name_mutex))
+    {
+        pr_debug("operation was killed.");
+        return -EAGAIN;
+    }
+    result = set_object_name_internal(str);
+    mutex_unlock(&object_name_mutex);
+    return result;
+}
+
 /*
  * Return current characteristic of module.
  *
  * In our case - name of object.
+ * Should be executed under mutes taken.
  */
 
 const char* get_object_name_internal(void)
@@ -260,104 +188,3 @@ int set_object_name_internal(const char* new_name)
     return 0;
 }
 
-//Helper for read operation of file. 'As if' it reads file, contatining string.
-ssize_t string_operation_read(const char* str, char __user *buf, size_t count, 
-	loff_t *f_pos)
-{
-    //length of 'file'(include terminating '\0')
-    size_t size = strlen(str) + 1;
-    //whether position out of range
-    if((*f_pos < 0) || (*f_pos > size)) return -EINVAL;
-    if(*f_pos == size) return 0;// eof
-
-    if(count + *f_pos > size)
-        count = size - *f_pos;
-    if(copy_to_user(buf, str + *f_pos, count) != 0)
-        return -EFAULT;
-    
-    *f_pos += count;
-    return count;
-}
-
-//Helper for llseek operation of file. Help to user space utilities to find out size of file.
-loff_t string_operation_llseek (const char* str, loff_t *f_pos, loff_t offset, int whence)
-{
-    loff_t new_offset;
-    size_t size = strlen(str) + 1;
-    switch(whence)
-    {
-    case 0: /* SEEK_SET */
-        new_offset = offset;
-    break;
-    case 1: /* SEEK_CUR */
-        new_offset = *f_pos + offset;
-    break;
-    case 2: /* SEEK_END */
-        new_offset = size + offset;
-    break;
-    default: /* can't happen */
-        return -EINVAL;
-    };
-    if(new_offset < 0) return -EINVAL;
-    if(new_offset > size) new_offset = size;//eof
-    
-    *f_pos = new_offset;
-    //returning value is offset from the beginning, filp->f_pos, generally speaking, may be any.
-    return new_offset;
-}
-
-ssize_t
-string_operation_write(char** out_str, const char __user *buf,
-    size_t count, loff_t *f_pos)
-{
-    char* buffer;
-
-    if(count == 0)
-    {
-        pr_err("write: 'count' shouldn't be 0.");
-        return -EINVAL;
-    }
-
-    /*
-     * Feature of control files.
-     *
-     * Because writting to such files is really command to the module to do something,
-     * and successive reading from this file return total effect of this command.
-     * it is meaningless to process writting not from the start.
-     *
-     * In other words, writting always affect to the global content of the file.
-     */
-    if(*f_pos != 0)
-    {
-        pr_err("Partial rewritting is not allowed.");
-        return -EINVAL;
-    }
-    //Allocate buffer for writting value - for its preprocessing.
-    buffer = kmalloc(count + 1, GFP_KERNEL);
-    if(buffer == NULL)
-    {
-        pr_err("Cannot allocate buffer.");
-        return -ENOMEM;
-    }
-
-    if(copy_from_user(buffer, buf, count) != 0)
-    {
-        pr_err("copy_from_user return error.");
-        kfree(buffer);
-        return -EFAULT;
-    }
-    // For case, when one try to write not null-terminated sequence of bytes,
-    // or omit terminated null-character.
-    buffer[count] = '\0';
-
-    /*
-     * Usually, writting to the control file is performed via 'echo' command,
-     * which append new-line symbol to the writting string.
-     *
-     * Because, this symbol is usually not needed, we trim it.
-     */
-    if(buffer[count - 1] == '\n') buffer[count - 1] = '\0';
-
-    *out_str = buffer;
-    return count;
-}
